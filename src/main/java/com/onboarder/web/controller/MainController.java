ee1b3a7e89df43e7386e4b638b1a7c22bb5972b6
stymjs0515 <stymjs0515@gmail.com>
2018-05-07 15:11:28 +0900

    -onboarder library import

--- src/main/java/com/onboarder/web/controller/MainController.java
+++ src/main/java/com/onboarder/web/controller/MainController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,6 +21,7 @@ public class MainController {
     }
 
     @RequestMapping("/admin")
+    @PutMapping
     public String testAdmin(Model model) {
 
         LOGGER.debug("Admin View");
